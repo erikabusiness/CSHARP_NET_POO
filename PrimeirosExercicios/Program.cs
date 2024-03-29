243ffbad3b654b6109b8785d83357e39dfe68c95
Erika Alves <erikaalves.m.silva@gmail.com>
2024-03-29 16:00:02 -0300

    feat: classe Pessoa

---
+++ PrimeirosExercicios/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace PrimeirosExercicios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Pessoa Maria, Joao;
+            Maria = new Pessoa();
+            Joao = new Pessoa();
+
+            Funcionario funcionario1, funcionario2;
+            funcionario1 = new Funcionario();
+            funcionario2 = new Funcionario();
+
+            Pessoa[] nomes = { Maria, Joao };
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Insira o sobrenome de " + (i == 0 ? "Maria" : "João") + ":");
+                nomes[i].Nome = Console.ReadLine();
+                Console.WriteLine("Insira a idade de " + (i == 0 ? "Maria" : "João") + ":");
+                nomes[i].Idade = int.Parse(Console.ReadLine());
+            }
+
+            String verificarMaisVelho = (nomes[0].Idade > nomes[1].Idade ? ("Maria é mais velha!") : ("João é mais velho!"));
+            Console.WriteLine(verificarMaisVelho);
+
+            }
+
+
+
+        }
+
+
+    }
+}
--- PrimeirosExercicios/obj/Debug/net8.0/PrimeirosExercicios.AssemblyInfo.cs
+++ PrimeirosExercicios/obj/Debug/net8.0/PrimeirosExercicios.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("TrianguloHeron")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("PrimeirosExercicios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ebf83352bf64e5f659a128621ee20c5e5d3b235")]
-[assembly: System.Reflection.AssemblyProductAttribute("TrianguloHeron")]
-[assembly: System.Reflection.AssemblyTitleAttribute("TrianguloHeron")]
+[assembly: System.Reflection.AssemblyProductAttribute("PrimeirosExercicios")]
+[assembly: System.Reflection.AssemblyTitleAttribute("PrimeirosExercicios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
